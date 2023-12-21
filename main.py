df67533981f88b843d291d4549d407b19a556abd
Dmytro Boiko <dmytroboiko007@gmail.com>
2023-12-21 10:56:30 +0200

    add instructions to endpoints

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ app = FastAPI()
 
 
 class Todo(BaseModel):
+    """
+       Attributes:
+       - id: Unique identifier for the to-do item.
+       - title: Title of the to-do item.
+       - description: Description of the item.
+       - completed: Indicates whether the to-do item is completed or not.
+    """
     id: int
     title: str
     description: str
@@ -24,17 +31,44 @@ def get_todo_by_id(todo_id: int):
 
 @app.post("/todos/")
 def create_todo(todo: Todo):
+    """
+        Create a new to-do item.
+
+        Parameters:
+        - todo: The data for the new to-do item.
+
+        Returns:
+        - Todo: The newly created to-do item.
+    """
     todos_db.append(todo)
     return todo
 
 
 @app.get("/todos/", response_model=List[Todo])
 def read_todos():
+    """
+        Get a list of all to-do items.
+
+        Returns:
+        - List[Todo]: List of all to-do items.
+    """
     return todos_db
 
 
 @app.get("/todos/{todo_id}", response_model=Todo)
 def read_todo(todo_id: int):
+    """
+        Get information about a specific to-do item.
+
+        Parameters:
+        - todo_id: The ID of the to-do item.
+
+        Returns:
+        - Todo: Information about the to-do item.
+
+        Raises:
+        - HTTPException 404: If the to-do item with the specified ID is not found.
+    """
     todo = get_todo_by_id(todo_id)
     if todo is None:
         raise HTTPException(status_code=404, detail="Todo not found")
@@ -43,6 +77,19 @@ def read_todo(todo_id: int):
 
 @app.put("/todos/{todo_id}", response_model=Todo)
 def update_todo(todo_id: int, completed: bool):
+    """
+        Update the completion status of a to-do item.
+
+        Parameters:
+        - todo_id: The ID of the to-do item to be updated.
+        - completed: The new completion status.
+
+        Returns:
+        - Todo: The updated to-do item.
+
+        Raises:
+        - HTTPException 404: If the to-do item with the specified ID is not found.
+    """
     todo = get_todo_by_id(todo_id)
     if todo is None:
         raise HTTPException(status_code=404, detail="Todo not found")
@@ -52,6 +99,18 @@ def update_todo(todo_id: int, completed: bool):
 
 @app.delete("/todos/{todo_id}", response_model=Todo)
 def delete_todo(todo_id: int):
+    """
+        Delete a to-do item.
+
+        Parameters:
+        - todo_id: The ID of the to-do item to be deleted.
+
+        Returns:
+        - Todo: The deleted to-do item.
+
+        Raises:
+        - HTTPException 404: If the to-do item with the specified ID is not found.
+    """
     todo = get_todo_by_id(todo_id)
     if todo is None:
         raise HTTPException(status_code=404, detail="Todo not found")
